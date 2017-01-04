aabf5c9d67b8d1de14d83c4cbd1380a351d2543e
Topher Hunt <hunt.topher@gmail.com>
2017-01-04 13:26:34 -0500

    Prevent session timeouts from triggering InvalidAuthenticityTokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     work_entries_path
   end
+
+  # Prevent timeouts from causing the app to freak out, that's just unprofessional
+  rescue_from ActionController::InvalidAuthenticityToken do |e|
+    logger.error "A user just got ActionController::InvalidAuthenticityToken #{e} on request #{request.method} #{request.original_url}. Redirecting to the login page."
+    redirect_to new_user_session_path, alert: "Whoops, it looks like your session timed out. Please log in and try again."
+  end
 end

a5b577e08cafbe39ac01b84a4fccae74198f2672
Topher Hunt <hunt.topher@gmail.com>
2017-07-07 17:15:07 -0500

    Fix bug with entry edit page (project wasnt selected)

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,33 +1,33 @@
 module ProjectsHelper
   def project_options_for_select(opts = {})
-    Cacher.fetch("user_#{current_user.id}_project_options", expires_in: 1.day) do
-      options = []
-      options += [[opts[:root].to_s, nil]] if opts[:root]
-      current_user.projects.top_level.order(:name).each do |project|
-        entries = arrays_for_project_and_children(project, opts)
-        options += entries if entries
-      end
-      # eg. [
-      #   ['parent1', 1],
-      #   ['  child', 2],
-      #   ['parent2', 3],
-      #   ['  child', 4] ]
-
-      options_for_select(options, opts[:selected].to_i)
+    options = []
+    options += [[opts[:root].to_s, nil]] if opts[:root]
+    current_user.projects.top_level.order(:name).each do |project|
+      entries = arrays_for_project_and_children(project, opts)
+      options += entries if entries
     end
+    # eg. [
+    #   ['parent1', 1],
+    #   ['parent1: child', 2],
+    #   ['parent2', 3],
+    #   ['parent2: child', 4] ]
+
+    options_for_select(options, opts[:selected].to_i)
   end
 
   def arrays_for_project_and_children(project, opts)
-    return nil if opts[:exclude] and opts[:exclude] == project
-    return nil if opts[:exclude_inactive] and ! project.active? and opts[:selected].to_i != project.id
+    Cacher.fetch("project_#{project.id}_options_array", expires_in: 1.day) do
+      return nil if opts[:exclude] and opts[:exclude] == project
+      return nil if opts[:exclude_inactive] and ! project.active? and opts[:selected].to_i != project.id
 
-    output = [ name_and_id_for_this_project(project, opts) ]
-    project.children.order(:name).each do |child|
-      entries = arrays_for_project_and_children(child, opts)
-      output += entries if entries
+      output = [ name_and_id_for_this_project(project, opts) ]
+      project.children.order(:name).each do |child|
+        entries = arrays_for_project_and_children(child, opts)
+        output += entries if entries
+      end
+      output
+      # eg. [ ['parent', 1], ['  child', 2] ]
     end
-    output
-    # eg. [ ['parent', 1], ['  child', 2] ]
   end
 
   def name_and_id_for_this_project(project, opts)

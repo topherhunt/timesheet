a8174057bf5cc2be2e9ca6c141bb93a25c60db31
Topher Hunt <hunt.topher@gmail.com>
2018-06-02 21:38:22 +0200

    Fix Heroku deployment build error

--- config/initializers/clear_cache.rb
+++ config/initializers/clear_cache.rb
@@ -1,3 +1,4 @@
+`mkdir -p tmp/cache` # just in case this directory doesn't exist yet
 Rails.cache.clear
 Rails.logger.info "** Cleared the Rails cache."
 puts "** Cleared the Rails cache." if Rails.env.development?

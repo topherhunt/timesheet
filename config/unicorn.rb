1751c8319a7439bbf782e932d388ec9fdc56395d
Topher Hunt <hunt.topher@gmail.com>
2019-01-13 15:10:01 +0100

    Fix Unicorn sigterm bug

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,7 +7,7 @@ preload_app true
 before_fork do |server, worker|
   Signal.trap('TERM') do
     puts "Unicorn master intercepting TERM and sending myself QUIT instead."
-    Process.kill 'QUIT, Process.id'
+    Process.kill 'QUIT', Process.pid
   end
 
   ActiveRecord::Base.connection.disconnect!
